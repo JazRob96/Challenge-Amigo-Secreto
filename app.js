64db66d0a57614fa51c5d07fb53abea08582eec6
JazRob96 <jaxmin_20_96@hotmail.com>
2025-08-17 21:25:06 -0500

    Actualización array

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos =[Mariana,Leo];
-console.log(amigos);
+//1. Ingresar un nombre al array (al hacer clic en boton añadir)
+//2.Imprimer en espacio lista amigos
+
+let Amigos =[];
+
+function agregarAmigo(){
+    let friend;
+    friend = document.getElementById('amigo').value;                   
+    if(friend===""){
+        alert('Por favor, inserte un nombre.');
+    return false;       
+    }
+    Amigos.push(friend);   
+    console.log(Amigos); 
+}
+
