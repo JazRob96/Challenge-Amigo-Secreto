8a85097db4ec3d55d1f362e44af20021b016e333
JazRob96 <jaxmin_20_96@hotmail.com>
2025-08-23 21:46:04 -0500

    Actualizacion fx agregarAmigo() y sortearAmigo()

--- app.js
+++ app.js
@@ -2,16 +2,53 @@
 //1. Ingresar un nombre al array (al hacer clic en boton añadir)
 //2.Imprimer en espacio lista amigos
 
-let Amigos =[];
+let amigos =[];
+function limpiarCaja(){
+    //el uso de # es porque en si nuestro identificador es un id del HTML al que le hemos puesto valorUsuario para diferenciarlo del resto
+    //let valorCaja=document.querySelector('#valorUsuario');
+    //valorCaja.value ='';
+    document.querySelector('#amigo').value='';
+}
 
+//Tomemos en cuenta que el agregarAmigo(), se ejecuta cada que se presiona
+// el botón (button)
 function agregarAmigo(){
-    let friend;
-    friend = document.getElementById('amigo').value;                   
+    //1.Agregar nombres cada que se presione el boton "Añadir"
+    
+    let friend = document.getElementById('amigo').value;                   
     if(friend===""){
         alert('Por favor, inserte un nombre.');
     return false;       
     }
-    Amigos.push(friend);   
-    console.log(Amigos); 
+
+    amigos.push(friend);   
+    console.log(amigos);
+
+    listaAmigos.innerHTML = "";
+
+    //2. Recorre el arreglo creado y asignar 
+
+    for(let i=0; i<amigos.length;i++){
+        //Crear etiqueta li(espacio) para(for) i=0 (que es el primer ingreso)
+        let li = document.createElement("li");
+        //Agrego el amigos(0) a la etiqueta li creada
+        li.innerHTML=`${i+1} . ${amigos[i]}`;
+        //Agregar la etiqueta li creada con su contenido en el bloque (lista amigos)
+        listaAmigos.appendChild(li);
+    }
+    
+    limpiarCaja();      
+    
 }
+
+function sortearAmigo(){
+    if(amigos.length===0){
+        alert('Por favor, insertar nombres para el sorteo');
+    return false;       
+    }            
+    let seleccion = document.getElementById('resultado');
+    seleccion.innerHTML=amigos[Math.floor(Math.random() * amigos.length)];
+    console.log(seleccion.innerHTML);    
+}
+
 
